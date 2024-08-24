fcf631da1726b3f49fc093be566d9d98376cd39f
pinwheeeel <phil.huang@student.tdsb.on.ca>
2024-08-24 16:15:09 -0400

    code cleanup

--- core/management/commands/add_events.py
+++ core/management/commands/add_events.py
@@ -14,30 +14,26 @@ from core.models import Event, Organization, Term
 SECRET_CAL_ADDRESS = "Change me"
 
 class Command(BaseCommand):
-    # TODO: change help text
-    help = "Adds organizations from Google Sheets. Does not modify existing organizations. See https://github.com/wlmac/metropolis/issues/247"
+    help = "Imports events that that have not yet ended from Google Calendar. See https://github.com/wlmac/metropolis/issues/250"
 
     def handle(self, *args, **options):
         if SECRET_CAL_ADDRESS == "Change me":
-            raise AssertionError("SECRET_CAL_ADDRESS is not set.")
-            # TODO: raise AssertionError("SECRET_CAL_ADDRESS is not set. Please change in metropolis/settings.py")
+            raise AssertionError("SECRET_CAL_ADDRESS is not set. Please change in metropolis/settings.py")
 
-        rq = requests.get(SECRET_CAL_ADDRESS)
+        response = requests.get(SECRET_CAL_ADDRESS)
 
-        if rq.status_code != 200:
-            raise AssertionError(f"Error {rq.status_code}, {rq.reason}. Could not get calendar data. Are you sure the calendar link is set correctly? See the instructions in core/management/commands/add_events.py")
-
-        lines = rq.text.splitlines()
+        if response.status_code != 200:
+            raise AssertionError(
+                f"Error {response.status_code}, {response.reason}. Could not get calendar data. Are you sure the calendar link is set correctly? See the instructions in core/management/commands/add_events.py"
+            )
 
         event_data = {}
-        is_in_event = False
+        in_event = False
 
-        for l in lines:
-            # self.stdout.write(l)
-            l = l.strip()
+        for line in response.text.splitlines():
+            line = line.strip()
 
-            if l == "BEGIN:VEVENT":
-                # Default values
+            if line == "BEGIN:VEVENT":
                 event_data = {
                     "name": "Unnamed Event",
                     "description": "",
@@ -49,178 +45,36 @@ class Command(BaseCommand):
                     "is_instructional": True,
                     "is_public": True,
                     "should_announce": False,
-                    # "tags": [],
                 }
-                is_in_event = True
-            
-            elif l == "END:VEVENT":
-                is_in_event = False
+                in_event = True
 
-                # Because past events are not deleted from the calendar, we don't
+            elif line == "END:VEVENT":
+                in_event = False
+
+                # Because past events are not deleted from the .ics file, we don't
                 # add any events that have already passed to prevent duplication.
                 if datetime.now() > event_data["end_date"]:
                     self.stdout.write(
-                        self.style.ERROR(f"Event '{event_data["name"]}' skipped because it has already passed.")
+                        self.style.ERROR(
+                            f"\nEvent '{event_data["name"]}' skipped because it has already passed."
+                        )
                     )
                     continue
 
                 self.stdout.write(
                     self.style.SUCCESS(f"\nNew event created:")
                 )
-                # self.stdout.write(f"\tData: {event_data}\n")
-                for k, v in event_data.items():
-                    self.stdout.write(f"\t{k}: {v}\n")
 
-                term = None
-                self.stdout.write(
-                    "\n\tPlease enter the name of the event's affiliated term (case insensitive):"
-                )
-                while True:
-                    try:
-                        print("\t", end="")
-                        term_name = input()
-                        term = Term.objects.get(name__iexact=term_name)
+                for key, value in event_data.items():
+                    self.stdout.write(f"\t{key}: {value}\n")
 
-                        self.stdout.write(
-                            self.style.SUCCESS(f"\tTerm found: {term}")
-                        )
-                        break
-                    except Term.DoesNotExist:
-                        self.stdout.write(
-                            self.style.ERROR(
-                                "\tTerm not found. Did you make a typo?"
-                            )
-                        )
-                        self.stdout.write("\tPlease re-enter term name:")
-                event_data["term"] = term
+                event_data["term"] = self._get_term()
+                event_data["organization"] = self._get_organization()
+                event_data["schedule_format"] = self._get_schedule_format()
 
-                organization = None
-                self.stdout.write(
-                    "\n\tPlease enter the name OR slug of the event's affiliated organization (case insensitive):"
-                )
-                while True:
-                    try:
-                        print("\t", end="")
-                        org_name = input()
-                        organization = Organization.objects.get(name__iexact=org_name)
-                        self.stdout.write(
-                            self.style.SUCCESS(
-                                f"\tOrganization found: {organization}"
-                            )
-                        )
-                        break
+                for key in ["is_instructional", "is_public", "should_announce"]:
+                    event_data[key] = self._get_boolean(key, event_data[key])
 
-                    except Organization.DoesNotExist:
-                        try:
-                            organization = Organization.objects.get(slug__iexact=org_name)
-                            self.stdout.write(
-                                self.style.SUCCESS(
-                                    f"\tOrganization found: {organization}"
-                                )
-                            )
-                            break
-
-                        except Organization.DoesNotExist:
-                            self.stdout.write(
-                                self.style.ERROR(
-                                    "\tOrganization not found. Did you make a typo?"
-                                )
-                            )
-                            self.stdout.write("\tPlease re-enter organization name:")
-                event_data["organization"] = organization
-
-                schedule_format = "default"
-                while True:
-                    self.stdout.write(
-                        f"\n\tChange schedule format from its default value ({event_data["schedule_format"]})? (y/N):"
-                    )
-                    print("\t", end="")
-
-                    i = input()
-
-                    if i.lower() == "y":
-                        options = [
-                            "early-dismissal",
-                            "one-hour-lunch",
-                            "early-early-dismissal",
-                            "default",
-                            "half-day",
-                            "late-start",
-                            "sac-election",
-                        ]
-
-                        for i, option in enumerate(options):
-                            self.stdout.write(f"\t{i+1}: {option}")
-                        
-                        while True:
-                            self.stdout.write("\tSelect a new format by number:")
-
-                            print("\t", end="")
-                            num = input()
-                            try:
-                                if int(num) > 0 and int(num) <= len(options):
-                                    schedule_format = options[int(num) - 1]
-                                    self.stdout.write(
-                                        self.style.SUCCESS(
-                                            f"\tNew value: {event_data["schedule_format"]}"
-                                        )
-                                    )
-                                    break
-                                else:
-                                    raise ValueError
-
-                            except ValueError:
-                                    self.stdout.write(
-                                        self.style.ERROR(
-                                            "\tInvalid input. Please enter a number within the list."
-                                        )
-                                    )
-                        break
-                    
-                    elif i.lower() == "n" or i.lower() == "":
-                        self.stdout.write(
-                            self.style.SUCCESS(f"\tNo change")
-                        )
-                        break
-
-                    else:
-                        self.stdout.write(
-                            self.style.ERROR(
-                                "\tInvalid input. Please enter 'y' or 'n' (leave blank for 'n')."
-                            )
-                        )
-                event_data["schedule_format"] = schedule_format
-
-                for s in ["is_instructional", "is_public", "should_announce"]:
-                    boolean = event_data[s]
-                    while True:
-                        self.stdout.write(
-                            f"\n\tChange {s} from its default value ({boolean})? (y/N):"
-                        )
-                        print("\t", end="")
-
-                        i = input()
-
-                        if i.lower() == "y":
-                            boolean = not boolean
-                            self.stdout.write(
-                                self.style.SUCCESS(f"\tNew value: {boolean}")
-                            )
-                            break
-                        
-                        elif i.lower() == "n" or len(i) == 0:
-                            self.stdout.write(
-                                self.style.SUCCESS(f"\tNo change")
-                            )
-                            break
-
-                        else:
-                            self.stdout.write(
-                                self.style.ERROR(
-                                    "\tInvalid input. Please enter 'y' or 'n' (leave blank for 'n')."
-                                )
-                            )
-                
                 event = Event(
                     name=event_data["name"],
                     description=event_data["description"],
@@ -235,30 +89,160 @@ class Command(BaseCommand):
                 )
                 event.save()
 
-                self.stdout.write(self.style.SUCCESS(f"\nEvent saved: {event}"))
-            
-            elif is_in_event:
-                if l.startswith("SUMMARY:"):
-                    event_data["name"] = l[len("SUMMARY:"):]
+                self.stdout.write(self.style.SUCCESS(f"\n\tEvent saved: {event}"))
 
-                elif l.startswith("DESCRIPTION:"):
-                    event_data["description"] = l[len("DESCRIPTION:"):]
+            elif in_event:
+                if line.startswith("SUMMARY:"):
+                    event_data["name"] = line[len("SUMMARY:"):]
+                elif line.startswith("DESCRIPTION:"):
+                    event_data["description"] = line[len("DESCRIPTION:"):]
+                elif line.startswith("DTSTART"):
+                    if line.startswith("DTSTART;VALUE=DATE:"):
+                        start_date = line[len("DTSTART;VALUE=DATE:"):]
+                        event_data["start_date"] = datetime.strptime(start_date, "%Y%m%d")
+                    elif line.startswith("DTSTART:"):
+                        start_date = line[len("DTSTART:"):]
+                        event_data["start_date"] = datetime.strptime(start_date, "%Y%m%dT%H%M%SZ")
+                elif line.startswith("DTEND"):
+                    if line.startswith("DTEND;VALUE=DATE:"):
+                        end_date = line[len("DTEND;VALUE=DATE:"):]
+                        event_data["end_date"] = datetime.strptime(end_date, "%Y%m%d")
+                    elif line.startswith("DTEND:"):
+                        end_date = line[len("DTEND:"):]
+                        event_data["end_date"] = datetime.strptime(end_date, "%Y%m%dT%H%M%SZ")
 
-                elif l.startswith("DTSTART"):
-                    if l.startswith("DTSTART;VALUE=DATE:"):
-                        dtstart = l[len("DTSTART;VALUE=DATE:"):]
-                        event_data["start_date"] = datetime.strptime(dtstart, "%Y%m%d") # TODO: fix timezone
+    def _get_yesno_response(self, question):
+        while True:
+            self.stdout.write(f"\t{question} (y/N):", ending="")
 
-                    elif l.startswith("DTSTART:"):
-                        dtstart = l[len("DTSTART:"):]
-                        event_data["start_date"] = datetime.strptime(dtstart, "%Y%m%dT%H%M%SZ") # TODO: fix timezone
+            i = input().lower()
 
-                elif l.startswith("DTEND"):
-                    if l.startswith("DTEND;VALUE=DATE:"):
-                        dtend = l[len("DTEND;VALUE=DATE:"):]
-                        event_data["end_date"] = datetime.strptime(dtend, "%Y%m%d") # TODO: fix timezone
+            if i == "y":
+                return True
+            elif i == "n" or i == "":
+                return False
+            else:
+                self.stdout.write(
+                    self.style.ERROR("Please enter 'y' or 'n' (leave blank for 'n')."), ending="\n\t"
+                )
 
-                    elif l.startswith("DTEND:"):
-                        dtend = l[len("DTEND:"):]
-                        event_data["end_date"] = datetime.strptime(dtend, "%Y%m%dT%H%M%SZ") # TODO: fix timezone
+    def _get_term(self):
+        self.stdout.write(f"\n\tPlease enter the name of the event's affiliated term (case insensitive): ", ending="\n\t")
 
+        while True:
+            term_name = input()
+            try:
+                term = Term.objects.get(name__iexact=term_name)
+
+                self.stdout.write(
+                    self.style.SUCCESS(f"\tTerm found: {term}")
+                )
+                return term
+
+            except Term.DoesNotExist:
+                self.stdout.write(
+                    self.style.ERROR(
+                        "\tTerm not found. Did you make a typo? Please try again."
+                    ), ending="\n\t"
+                )
+
+    def _get_organization(self):
+        self.stdout.write(f"\n\tPlease enter the name of the event's affiliated organization (case insensitive): ", ending="\n\t")
+        organization = None
+
+        while True:
+            organization_name = input()
+            try:
+                organization = Organization.objects.get(name__iexact=organization_name)
+
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"\tOrganization found: {organization}"
+                    )
+                )
+                break
+
+            except Organization.DoesNotExist:
+                try:
+                    organization = Organization.objects.get(slug__iexact=organization_name)
+
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"\tOrganization found: {organization}"
+                        )
+                    )
+                    break
+
+                except Organization.DoesNotExist:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            "\tOrganization not found. Did you make a typo? Please try again."
+                        ), ending="\n\t"
+                    )
+
+        return organization
+
+    def _get_schedule_format(self):
+        schedule_format = "default"
+        
+        if self._get_yesno_response(f"\n\tChange schedule format from its default value ({schedule_format})?"):
+            options = [
+                "early-dismissal",
+                "one-hour-lunch",
+                "early-early-dismissal",
+                "default",
+                "half-day",
+                "late-start",
+                "sac-election",
+            ]
+
+            self.stdout.write("\t------------------------")
+            for i, option in enumerate(options):
+                self.stdout.write(f"\t{i+1}: {option}")
+            self.stdout.write("\t------------------------")
+
+            while True:
+                self.stdout.write("\tSelect a new format by number: ", ending="")
+
+                i = input()
+
+                try:
+                    if int(i) > 0 and int(i) <= len(options):
+                        schedule_format = options[int(i) - 1]
+
+                        self.stdout.write(
+                            self.style.SUCCESS(
+                                f"\tNew value: {schedule_format}"
+                            )
+                        )
+                        break
+
+                    else:
+                        raise ValueError
+
+                except ValueError:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            "\tInvalid input. Please enter a number within the list."
+                        )
+                    )
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(f"\tNo change")
+            )
+        
+        return schedule_format
+
+    def _get_boolean(self, key, value):
+        if self._get_yesno_response(f"\n\tChange {key} from its default value ({value})?"):
+            self.stdout.write(
+                self.style.SUCCESS(f"\tNew value: {not value}")
+            )
+            return not value
+
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(f"\tNo change")
+            )
+            return value
+    
