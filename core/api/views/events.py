51e773dce64ac8534144dbc3919bc8238db19ec9
pinwheeeel <phil.huang@student.tdsb.on.ca>
2024-10-09 20:36:43 -0400

    erm what the sigma

--- core/api/views/events.py
+++ core/api/views/events.py
@@ -15,69 +15,28 @@ class EventsList(ListAPIViewWithFallback):
     pagination_class = None
 
     def get_queryset(self):
-        start = timezone.now()
-        if self.request.data.get("start"):
-            start = self.request.data.get("start")
-        elif self.request.query_params.get("start"):
-            start = self.request.query_params.get("start")
+        start = (
+            self.request.data.get("start")
+            or self.request.query_params.get("start")
+            or timezone.now()
+        )
+        end = self.request.data.get("end") or self.request.query_params.get("end")
+
+        events = models.Event.objects.filter(end_date__gte=start)
+
+        if end:
+            events = events.filter(start_date__lte=end)
 
         if not self.request.user.is_anonymous:
-            if self.request.data.get("end"):
-                end = self.request.data.get("end")
-                events = (
-                    models.Event.objects.filter(
-                        end_date__gte=start, start_date__lte=end
-                    )
-                    .filter(
-                        Q(is_public=True)
-                        | Q(organization__member=self.request.user.id)
-                        | Q(organization__supervisors=self.request.user.id)
-                        | Q(organization__execs=self.request.user.id)
-                    )
-                    .distinct()
-                    .order_by("start_date")
-                )
-            elif self.request.query_params.get("end"):
-                end = self.request.query_params.get("end")
-                events = (
-                    models.Event.objects.filter(
-                        end_date__gte=start, start_date__lte=end
-                    )
-                    .filter(
-                        Q(is_public=True)
-                        | Q(organization__member=self.request.user.id)
-                        | Q(organization__supervisors=self.request.user.id)
-                        | Q(organization__execs=self.request.user.id)
-                    )
-                    .distinct()
-                    .order_by("start_date")
-                )
-            else:
-                events = (
-                    models.Event.objects.filter(end_date__gte=start)
-                    .filter(
-                        Q(is_public=True)
-                        | Q(organization__member=self.request.user.id)
-                        | Q(organization__supervisors=self.request.user.id)
-                        | Q(organization__execs=self.request.user.id)
-                    )
-                    .distinct()
-                    .order_by("start_date")
-                )
+            events = events.filter(
+                Q(is_public=True)
+                | Q(organization__member=self.request.user.id)
+                | Q(organization__supervisors=self.request.user.id)
+                | Q(organization__execs=self.request.user.id)
+            ).distinct()
         else:
-            if self.request.data.get("end"):
-                end = self.request.data.get("end")
-                events = models.Event.objects.filter(
-                    end_date__gte=start, start_date__lte=end, is_public=True
-                ).order_by("start_date")
-            elif self.request.query_params.get("end"):
-                end = self.request.query_params.get("end")
-                events = models.Event.objects.filter(
-                    end_date__gte=start, start_date__lte=end, is_public=True
-                ).order_by("start_date")
-            else:
-                events = models.Event.objects.filter(
-                    end_date__gte=start, is_public=True
-                ).order_by("start_date")
+            events = events.filter(Q(is_public=True))
+
+        events = events.order_by("start_date")
 
         return events
